b6ac4d1c25d3c4fc831a9a8fcb61b65f1d25dc37
Keith Pickford <Keith@FunctionZero.com>
2019-06-30 18:27:49 +0100

    This library is now open-source under the MIT license.

--- MvvmZero/Commanding/GuardCommandAsync.cs
+++ MvvmZero/Commanding/GuardCommandAsync.cs
@@ -1,45 +1,34 @@
-﻿using System;
+﻿/*
+MIT License
+
+Copyright(c) 2016 - 2019 Function Zero Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.using System;
+*/
+
+using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace FunctionZero.MvvmZero.Commanding
 {
-
-#if false
-    public class DemoViewModel
-    {
-        public DemoViewModel()
-        {
-            this.DemoCommand = new Command<double>(async (x) => await CalculateSquareSlowly(x));
-        }
-
-        public ICommand DemoCommand { get; private set; }
-
-        private async Task<double> CalculateSquareSlowly(double number)
-        {
-            await Task.Delay(2000);
-            return number * number;
-        }
-    }
-
-    public class DemoViewModel2
-    {
-        public DemoViewModel2()
-        {
-            this.DemoCommand = new Command(async () => await CalculateSquareSlowly(10));
-        }
-
-        public ICommand DemoCommand { get; private set; }
-
-        private async Task<double> CalculateSquareSlowly(double number)
-        {
-            await Task.Delay(2000);
-            return number * number;
-        }
-    }
-#endif
-
     public class GuardCommandAsync : ICommand
     {
         private readonly IGuard _guard;
@@ -109,8 +98,6 @@ namespace FunctionZero.MvvmZero.Commanding
                 }
             }
         }
-
-
 
         public void ChangeCanExecute()
         {
