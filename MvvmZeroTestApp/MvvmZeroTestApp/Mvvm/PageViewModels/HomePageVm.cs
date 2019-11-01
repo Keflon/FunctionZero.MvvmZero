5d51340f61721ddca56d54f1dc3b519e4173e756
Keith Pickford <Keith@FunctionZero.com>
2019-11-01 15:09:23 +0000

    Added BluePill animation.

--- MvvmZeroTestApp/MvvmZeroTestApp/Mvvm/PageViewModels/HomePageVm.cs
+++ MvvmZeroTestApp/MvvmZeroTestApp/Mvvm/PageViewModels/HomePageVm.cs
@@ -26,18 +26,23 @@ namespace MvvmZeroTestApp.Mvvm.PageViewModels
         private async Task RedPillCommandExecute()
         {
             await DoSomethingSilly();
-
             await App.Locator.PageService.PushPageAsync(PageDefinitions.RedPillPage, new RedPillPageVm());
         }
 
         private async Task DoSomethingSilly()
         {
-            //Simple harmonic motion.
-            Stopwatch s = new Stopwatch();
-            s.Start();
-            double time = 0.0;
+            // Approximate simple harmonic motion.
+            // MAGIC NUMBERS!!
+            AnchorX = 0;
+            AnchorY = 0;
+
+            double time;
             double amplitude = 30;
             double damping = 0.995;
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
             while (amplitude > 8)
             {
                 time = s.ElapsedMilliseconds / 1000.0;
@@ -47,6 +52,8 @@ namespace MvvmZeroTestApp.Mvvm.PageViewModels
             }
 
             s.Restart();
+            // Approximate gravity. TODO: Acceleration.
+            // MAGIC NUMBERS!!
             while (TranslationY < 500)
             {
                 time = s.ElapsedMilliseconds / 1000.0;
@@ -55,12 +62,34 @@ namespace MvvmZeroTestApp.Mvvm.PageViewModels
             }
         }
 
-
-
-
         private async Task BluePillCommandExecute()
         {
+            await DoSomethingElseSilly();
             await App.Locator.PageService.PushPageAsync(PageDefinitions.BluePillPage, new BluePillPageVm());
+        }
+
+        private async Task DoSomethingElseSilly()
+        {
+            // Approximate a cartoon balloon being deflated ...
+            // MAGIC NUMBERS!!
+            AnchorX = 0;
+            AnchorY = 0;
+
+            double duration = 3.0;
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            double time = 0.0;
+
+            while (time < duration)
+            {
+                time = s.ElapsedMilliseconds / 1000.0;
+                Rotation = (time *(time / duration) * (time / duration) * 720.0);
+                AnchorX = Math.Sin(time*7.5);// * 10;
+                AnchorY = Math.Cos(time*7.5);// * 10;
+                Scale = 1 + Math.Sin((time / duration) * Math.PI/2.0*3.0);
+
+                await Task.Delay(15);
+            }
         }
     }
 }
--- MvvmZeroTestApp/MvvmZeroTestApp/Mvvm/ViewModels/BaseVm.cs
+++ MvvmZeroTestApp/MvvmZeroTestApp/Mvvm/ViewModels/BaseVm.cs
@@ -12,6 +12,15 @@ namespace MvvmZeroTestApp.Mvvm.ViewModels
         private double _translationX;
         private double _translationY;
         private double _rotation;
+        private double _scale;
+        private double _anchorX;
+        private double _anchorY;
+
+        public BaseVm()
+        {
+            // Something sensible ...
+            Scale = 1.0;
+        }
 
         public double TranslationX
         {
@@ -39,11 +48,48 @@ namespace MvvmZeroTestApp.Mvvm.ViewModels
         }
         public double Rotation
         {
-            get => _rotation; set
+            get => _rotation;
+            set
             {
                 if (value != _rotation)
                 {
                     _rotation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public double Scale
+        {
+            get => _scale;
+            set
+            {
+                if (value != _scale)
+                {
+                    _scale = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public double AnchorX
+        {
+            get => _anchorX;
+            set
+            {
+                if (value != _anchorX)
+                {
+                    _anchorX = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public double AnchorY
+        {
+            get => _anchorY;
+            set
+            {
+                if (value != _anchorY)
+                {
+                    _anchorY = value;
                     OnPropertyChanged();
                 }
             }
@@ -54,22 +100,27 @@ namespace MvvmZeroTestApp.Mvvm.ViewModels
             base.OwnerPageAppearing(pageKey, pageDepth);
 
             // HACK: Workaround for UWP BUG setting transform to 0.0 has no effect.
-            Rotation = 0.1;
+            //Rotation = 0.1;
             Rotation = 0.0;
             TranslationX = 0.1;
             TranslationX = 0.0;
             TranslationY = 0.1;
             TranslationY = 0.0;
+            AnchorX = 0.0;
+            AnchorY = 0.0;
+            Scale = 1.0;
         }
         public override void OwnerPageDisappearing()
         {
             base.OwnerPageDisappearing();
 
+            // iOS NavigationPage animates differently to UWP and Droid.
             if (Device.RuntimePlatform == Device.iOS)
             {
                 Rotation = 0.0;
                 TranslationX = 0.0;
                 TranslationY = 0.0;
+                Scale = 1.0;
             }
         }
     }
