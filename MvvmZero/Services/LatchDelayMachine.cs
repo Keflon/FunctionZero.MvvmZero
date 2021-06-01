210a019d6838662378239d39f0feb1c91f771114
Keith Pickford <Keith@FunctionZero.com>
2021-06-01 21:12:33 +0100

    Added LatchDelayMachine. Updated to zBind 1.2.0

---
+++ MvvmZero/Services/LatchDelayMachine.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FunctionZero.MvvmZero.Services
+{
+    public class LatchDelayMachine
+    {
+        private readonly TimeSpan _clockTimespan;
+        private readonly int _clockTicksBeforeAction;
+        private readonly Func<Task> _delayedActionAsync;
+        private readonly Func<Task> _delayStartedActionAsync;
+        private readonly Func<int, double, Task> _clockTickAsync;
+
+        private int _counter;
+        bool _timerIsRunning;
+
+        //public delegate Task DelayedActionAsync();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="millisecondClock"></param>
+        /// <param name="clockTicksBeforeAction"></param>
+        /// <param name="delayedActionAsync"></param>
+        /// <param name="delayStartedActionAsync"></param>
+        /// <param name="clockTickAsync"></param>
+        public LatchDelayMachine(
+            int millisecondClock,
+            int clockTicksBeforeAction,
+            Func<Task> delayedActionAsync,
+            Func<Task> delayStartedActionAsync = null,
+            Func<int, double, Task> clockTickAsync = null
+            )
+        {
+            _clockTimespan = new TimeSpan(0, 0, 0, 0, millisecondClock);
+            _clockTicksBeforeAction = clockTicksBeforeAction;
+            _delayedActionAsync = delayedActionAsync ?? (async () => { });
+            _delayStartedActionAsync = delayStartedActionAsync ?? (async () => { });
+            _clockTickAsync = clockTickAsync ?? (async (count, progress) => { });
+        }
+
+
+        /// <summary>
+        /// Calling this starts or resets the timer whose expiry raises delayedActionAsync
+        /// </summary>
+        /// <returns>False if an existing timer was reset; true if no timer was running and a new one was created.</returns>
+        public bool Poke()
+        {
+            _counter = 0;
+
+            if (!_timerIsRunning)
+            {
+                _timerIsRunning = true;
+                _ = _delayStartedActionAsync?.Invoke();
+                _ = _clockTickAsync(_counter, _counter / _clockTicksBeforeAction);
+                Device.StartTimer(_clockTimespan, AreWeReadyToRock);
+                return true;
+            }
+            return false;
+        }
+
+        private bool AreWeReadyToRock()
+        {
+            _counter++;
+            _clockTickAsync(_counter, _counter / _clockTicksBeforeAction);
+
+            if (_counter >= _clockTicksBeforeAction)
+            {
+                _timerIsRunning = false;
+                _ = _delayedActionAsync.Invoke();
+            }
+            return _timerIsRunning;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 You'll find a sample cross-platform application and a lovely tutorial [here](https://github.com/Keflon/MvvmZeroTutorialApp)
 
+MvvmZero can now bind to expressions, like this:
+```xaml
+IsVisible={z:Bind Items.Count != 0}
+```
+For more information, look [here](https://github.com/Keflon/FunctionZero.zBindTestApp)
+
